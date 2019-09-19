c4e066e58a310d8af21e40b51b3a017f7a7f2ebf
Artem Ulko <ulkoart@gmail.com>
2019-09-19 14:42:28 +0300

    rm knockKnock button

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -25,18 +25,5 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
         
     }
-    
-    @IBAction func knockKnock(){
-        let alert = UIAlertController(title: "Knock Knock!", message: "Who`s There!", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title:"Little old lady", style: .default, handler: nil)
-        
-        alert.addAction(action)
-        
-        present(alert, animated: true, completion: nil)
-        
-    }
-
-
 }
 
