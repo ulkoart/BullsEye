eebbc9a952a348d11c978749d69afb99eeca58c7
ulkoart <ulkoart@gmail.com>
2019-09-19 20:40:34 +0300

    add startNewRound method

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue: Int = 0
     
     @IBOutlet weak var slider: UISlider!
 
@@ -18,11 +19,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+        self.startNewRound()
+//        or startNewRound() without self
     }
     
     @IBAction func showHitMeAlert(){
         
-        let message = "The value of the slider is now: \(currentValue)"
+        let message = "The value of the slider is now: \(currentValue)" +
+        "\n The target value is: \(targetValue)"
         
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
         
@@ -32,11 +36,19 @@ class ViewController: UIViewController {
         
         present(alert, animated: true, completion: nil)
         
+        self.startNewRound()
+        
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 }
 
